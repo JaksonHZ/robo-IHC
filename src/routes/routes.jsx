ef10b082c5225b134dae3fa8470b1a0474160951
JaksonHZ <jaksonhuangz@gmail.com>
2024-01-23 17:00:57 -0300

    feat: iniciando a pagina de respiração

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Reconhecimento from "../pages/Reconhecimento";
+import Respiracao from "../pages/Respiracao";
 const AppRouter = () => {
   return(
     <>
       <Routes>
         <Route path="/reconhecimento" element={<Reconhecimento />}/>
+        <Route path="/respiracao" element={<Respiracao />}/>
       </Routes> 
     </>
   )
