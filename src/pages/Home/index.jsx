322bc15ae78b2818b80ae9a1fd06ead1e016bd40
Gustavo da Silva Garcia <gustavodsgarcia@gmail.com>
2024-01-30 19:52:18 -0300

    modifiquei a pagina inicial e adicionei a pagina termos de uso

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import styles from "./styles.module.css";
 import CustomFontSize from "../../components/CustomFontSize/CustomFontSize";
 import { FontSizeContext } from "../../context/ContextFontSize";
+import { Link } from "react-router-dom";
 
 export default function Home(){
     const { fontSize } = useContext(FontSizeContext);
@@ -13,9 +14,10 @@ export default function Home(){
             <h1 style={{ fontSize: `${fontSize}px` }}>Eu sou o robô anfitrião do DC!</h1>
             <h1 style={{ fontSize: `${fontSize}px` }}>Gostaria de iniciar um atendimento?</h1>
             <img src="src\assets\bot.png" alt="Não foi possível encontrar a imagem :(" />
-            <input type="button" value="INICIAR" className={styles.button} />
-            <p>Ao clicar em INICIAR você estará concordando com os termos de uso</p>
+            <Link to="/reconhecimento" style={{ textDecoration: 'none' }}>
+                <input type="button" value="INICIAR" className={styles.button} />
+            </Link>
+            <p>Ao clicar em INICIAR você estará concordando com os <Link to="/termos_de_uso" style={{ color: 'white' }}>Termos de Uso</Link>.</p>
         </div>
-
-    )
+    );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import FeedbackReconhecimento from "../pages/FeedbackReconhecimento";
 import RespostasProntas from "../pages/RespostasProntas";
 import ProblemasNosEstudos from "../pages/ProblemasNosEstudos";
+import TermosDeUso from "../pages/TermosDeUso";
 
 const AppRouter = () => {
   return(
@@ -16,6 +17,7 @@ const AppRouter = () => {
         <Route path="/reconhecimento/feedback" element={<FeedbackReconhecimento />}/>
         <Route path="/respostas_prontas" element={<RespostasProntas />}/>
         <Route path="/problemas_nos_estudos" element={<ProblemasNosEstudos />}/>
+        <Route path="/termos_de_uso" element={<TermosDeUso />}/>
       </Routes> 
     </>
   )
