6c921637c01b729886844801c1ce8d821f8ba533
Gustavo da Silva Garcia <gustavodsgarcia@gmail.com>
2024-02-02 13:44:49 -0300

    modificado tela respostas_prontas

--- src/pages/ProblemasNosEstudos/index.jsx
+++ src/pages/ProblemasNosEstudos/index.jsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 import CustomFontSize from "../../components/CustomFontSize/CustomFontSize";
 import { FontSizeContext } from "../../context/ContextFontSize";
 
 export default function ProblemasNosEstudos(){
     const { fontSize } = useContext(FontSizeContext);
+    
+    const calculateDynamicFontSize = (baseFontSize) => {
+        // Aqui você pode ajustar o fator multiplicativo ou aditivo conforme necessário
+        return baseFontSize * (fontSize / 40);
+      };
 
     return(
         <div className={styles.body}>
             <CustomFontSize />
             <h1 className={styles.firstSon} style={{ fontSize: `${fontSize}px` }}>Estou tendo problemas nos meus estudos da faculdade</h1>
-            <div className={styles.button}>
+            <div className={styles.button} style={{ fontSize: `${calculateDynamicFontSize(16)}px` }}>
                 <p>Você conhece o ProEstudo?</p>
                 <p>O objetivo principal do ProEstudo é apoiar os alunos de graduação</p>
                 <p>da UFSCar em desenvolver e aprimorar seu repertório de estudo.</p>
@@ -22,7 +28,14 @@ export default function ProblemasNosEstudos(){
                 <img src="src/assets/proestudo.png" alt="ProEstudo" className={styles.proestudoImage} />
                 <p>@proestudo_ufscar</p>
                 <p>proestudo@ufscar.br</p>
+                
+                <div className={styles.allback}>
+            <Link to="/respostas_prontas" style={{ textDecoration: 'none' }}>
+                  <input type="button" value="VOLTAR" className={styles.back} style={{ fontSize: `${calculateDynamicFontSize(16)}px` }} />
+            </Link>
+          </div>
             </div>
+            
         </div>
 
     )
--- src/pages/RespostasProntas/index.jsx
+++ src/pages/RespostasProntas/index.jsx
@@ -1,19 +1,41 @@
 import { useContext } from "react";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 import CustomFontSize from "../../components/CustomFontSize/CustomFontSize";
 import { FontSizeContext } from "../../context/ContextFontSize";
+import ConfigButton from "../../components/ConfigButton/ConfigButton";
 
 export default function RespostasProntas(){
     const { fontSize } = useContext(FontSizeContext);
 
+    const calculateDynamicFontSize = (baseFontSize) => {
+      // Aqui você pode ajustar o fator multiplicativo ou aditivo conforme necessário
+      return baseFontSize * (fontSize / 40);
+    };
+
     return(
         <div className={styles.body}>
             <CustomFontSize />
+            <ConfigButton />
             <h1 className={styles.firstSon} style={{ fontSize: `${fontSize}px` }}>O que mais te incomoda nesse momento?</h1>
-            <input type="button" value="ESTOU TENDO PROBLEMAS NOS MEUS ESTUDOS DA FACULDADE" className={styles.button} />
-            <input type="button" value="ESTOU TENDO PROBLEMAS FAMILIARES E ME SENTINDO SÓ" className={styles.button} />
-            <input type="button" value="ESTOU MUITO ANSIOSO NO AMBIENTE DA FACULDADE E ME SINTO PRESSIONADO" className={styles.button} />
-            <input type="button" value="NÃO SEI INFORMAR" className={styles.button} />
+            <Link to="/problemas_nos_estudos" style={{ textDecoration: 'none' }}>
+                <input style={{ fontSize: `${calculateDynamicFontSize(18)}px` }} type="button" value="ESTOU TENDO PROBLEMAS NOS MEUS ESTUDOS DA FACULDADE" className={styles.button} />
+            </Link>
+            <Link to="/sentindo_so" style={{ textDecoration: 'none' }}>
+                <input style={{ fontSize: `${calculateDynamicFontSize(18)}px` }} type="button" value="ESTOU TENDO PROBLEMAS FAMILIARES E ME SENTINDO SÓ" className={styles.button} />
+            </Link>
+            <Link to="/ansiedade" style={{ textDecoration: 'none' }}>
+                <input style={{ fontSize: `${calculateDynamicFontSize(18)}px` }} type="button" value="ESTOU MUITO ANSIOSO NO AMBIENTE DA FACULDADE E ME SINTO PRESSIONADO" className={styles.button} />
+            </Link>
+            <Link to="/semInfo" style={{ textDecoration: 'none' }}>
+                <input style={{ fontSize: `${calculateDynamicFontSize(18)}px` }} type="button" value="NÃO SEI INFORMAR" className={styles.button} />
+            </Link>
+
+            <div className={styles.allback}>
+            <Link to="/respostas_prontas" style={{ textDecoration: 'none' }}>
+                  <input type="button" value="VOLTAR" className={styles.back} style={{ fontSize: `${calculateDynamicFontSize(16)}px` }} />
+            </Link>
+          </div>
         </div>
 
     )
